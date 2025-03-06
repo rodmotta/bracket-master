d37956fa80ae67579bb93e69ea9bac33dee2e728
Rodrigo Motta <rodmotta777@gmail.com>
2025-03-05 23:35:24 -0300

    project creation

---
+++ src/main/java/com/github/rodmotta/bracket_master/BracketMasterApplication.java
@@ -0,0 +1,13 @@
+package com.github.rodmotta.bracket_master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BracketMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BracketMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/rodmotta/bracket_master/BracketMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rodmotta.bracket_master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BracketMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
